630c8dc31c02d9ca1bb67b6b3edeb7e6a589e57a
Grazfather <grazfather@gmail.com>
2017-05-21 13:19:31 -0700

    ndh: Clean up reading and writing values

--- go/cpu/ndh/ndh.go
+++ go/cpu/ndh/ndh.go
@@ -87,14 +87,12 @@ func (c *NdhCpu) setZf(v bool) {
 
 // execution
 func (c *NdhCpu) Start(begin, until uint64) error {
-	var pc uint64
 	var dis Dis
 	var jump uint64
-	var v uint16
-	var v2 uint16
 	c.stopRequest = false
-	c.RegWrite(PC, begin)
-	c.OnBlock(begin, 0)
+	var pc = begin
+	c.RegWrite(PC, pc)
+	c.OnBlock(pc, 0)
 
 	for pc != until && !c.stopRequest && c.err == nil {
 		pc, _ = c.RegRead(PC)
@@ -109,88 +107,90 @@ func (c *NdhCpu) Start(begin, until uint64) error {
 		}
 		instr := instructions[0].(*ins)
 		c.OnCode(pc, uint32(len(instr.bytes)))
+
 		if c.stopRequest {
 			break
 		}
+
+		var a, b arg
+		var va, vb uint16
+		switch len(instr.args) {
+		case 2:
+			b = instr.args[1]
+			fallthrough
+		case 1:
+			a = instr.args[0]
+		}
+		zfcheck := func(v uint16) uint16 {
+			if v == 0 {
+				c.RegWrite(ZF, 1)
+			} else {
+				c.RegWrite(ZF, 0)
+			}
+			return v
+		}
+
 		switch instr.op {
 		case OP_END:
 			return models.ExitStatus(0)
 		case OP_ADD:
-			v = c.get(instr.args[0]) + c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)+c.get(b)))
 		case OP_SUB:
-			v = c.get(instr.args[0]) - c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)-c.get(b)))
 		case OP_DIV:
-			v = c.get(instr.args[0]) / c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)/c.get(b)))
 		case OP_MUL:
-			v = c.get(instr.args[0]) * c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)*c.get(b)))
 		case OP_NOT:
-			v = c.get(instr.args[0]) ^ ^uint16(0)
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)^^uint16(0)))
 		case OP_OR:
-			v = c.get(instr.args[0]) | c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)|c.get(b)))
 		case OP_AND:
-			v = c.get(instr.args[0]) & c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)&c.get(b)))
 		case OP_XOR:
-			v = c.get(instr.args[0]) ^ c.get(instr.args[1])
-			c.set(instr.args[0], v)
-			c.setZf(v == 0)
+			c.set(a, zfcheck(c.get(a)^c.get(b)))
 		case OP_MOV:
-			v = c.get(instr.args[1])
-			c.set(instr.args[0], v)
+			c.set(a, c.get(b))
 		case OP_XCHG:
-			v = c.get(instr.args[0])
-			v2 = c.get(instr.args[1])
-			c.set(instr.args[0], v2)
-			c.set(instr.args[1], v)
+			va = c.get(a)
+			vb = c.get(b)
+			c.set(a, vb)
+			c.set(b, va)
 		case OP_INC:
-			v = c.get(instr.args[0]) + 1
-			c.set(instr.args[0], v)
+			c.set(a, c.get(a)+1)
 		case OP_DEC:
-			v = c.get(instr.args[0]) - 1
-			c.set(instr.args[0], v)
+			c.set(a, c.get(a)-1)
 		case OP_POP:
+			var v uint64
 			sp, _ := c.RegRead(SP)
-			v, _ := c.ReadUint(sp, 2, cpu.PROT_READ)
+			v, c.err = c.ReadUint(sp, 2, cpu.PROT_READ)
 			sp += 2
 			c.RegWrite(SP, sp)
-			c.set(instr.args[0], uint16(v))
+			c.set(a, uint16(v))
 		case OP_PUSH:
 			size := 2
-			v = c.get(instr.args[0])
+			v := c.get(a)
 			sp, _ := c.RegRead(SP)
-			if _, ok := instr.args[0].(*a8); ok {
+			if _, ok := a.(*a8); ok {
 				size = 1
 			}
 			sp -= uint64(size)
 			c.WriteUint(sp, size, cpu.PROT_WRITE, uint64(v))
 			c.RegWrite(SP, sp)
 		case OP_TEST:
-			v = c.get(instr.args[0])
-			v2 = c.get(instr.args[1])
-			c.setZf(v == 0 && v2 == 0)
+			va = c.get(a)
+			vb = c.get(b)
+			c.setZf(va == 0 && vb == 0)
 		case OP_CMP:
-			v = c.get(instr.args[0])
-			v2 = c.get(instr.args[1])
-			if v == v2 {
+			va = c.get(a)
+			vb = c.get(b)
+			if va == vb {
 				c.setZf(true)
 				c.RegWrite(AF, 0)
 				c.RegWrite(BF, 0)
 			} else {
 				c.setZf(false)
-				if v > v2 {
+				if va > vb {
 					c.RegWrite(AF, 1)
 					c.RegWrite(BF, 0)
 				} else {
@@ -201,22 +201,22 @@ func (c *NdhCpu) Start(begin, until uint64) error {
 		case OP_JMPS:
 			fallthrough
 		case OP_JMPL:
-			jump = uint64(c.get(instr.args[0]))
+			jump = uint64(c.get(a))
 		case OP_JZ:
 			if zf, _ := c.RegRead(ZF); zf == 1 {
-				jump = uint64(c.get(instr.args[0]))
+				jump = uint64(c.get(a))
 			}
 		case OP_JNZ:
 			if zf, _ := c.RegRead(ZF); zf != 1 {
-				jump = uint64(c.get(instr.args[0]))
+				jump = uint64(c.get(a))
 			}
 		case OP_JA:
 			if af, _ := c.RegRead(AF); af != 1 {
-				jump = uint64(c.get(instr.args[0]))
+				jump = uint64(c.get(a))
 			}
 		case OP_JB:
 			if bf, _ := c.RegRead(BF); bf != 1 {
-				jump = uint64(c.get(instr.args[0]))
+				jump = uint64(c.get(a))
 			}
 		case OP_SYSCALL:
 			c.OnIntr(0)
@@ -228,14 +228,14 @@ func (c *NdhCpu) Start(begin, until uint64) error {
 			c.WriteUint(sp, 2, cpu.PROT_WRITE, pc+uint64(len(instr.Bytes())))
 			// Call is special: If the arg is a register it's an
 			// absolute jump, otherwise it's an offset
-			v = c.get(instr.args[0])
-			switch instr.args[0].(type) {
+			va = c.get(a)
+			switch a.(type) {
 			case *reg:
-				c.RegWrite(PC, uint64(v))
-				c.OnBlock(uint64(v), 0)
+				c.RegWrite(PC, uint64(va))
+				c.OnBlock(uint64(va), 0)
 				continue
 			case *a16:
-				jump = uint64(v)
+				jump = uint64(va)
 			}
 		case OP_RET:
 			// Pop RA
